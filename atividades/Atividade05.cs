f307548176e9bf73860a2693415f51abfd4661ec
Ernane Sousa <ernane.computacao@gmail.com>
2023-09-23 17:28:43 -0300

    Adicionando atividade 05

--- atividades/Atividade05.cs
+++ atividades/Atividade05.cs
@@ -6,6 +6,17 @@ class Atividade05
     public static void Executar()
     {
 
-    }
+        Console.WriteLine("");
+        Console.WriteLine("Atividade 05 - Números ímpares");
 
+        int numeros = 50;
+
+        for (int i = 1; i <= numeros; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
 }
